e8c9704440fed345df87ab11a888de5e5f66ee37
Mahbub Alam <ju.mahbub@gmail.com>
2020-11-11 03:02:19 +0600

    2nd commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import AirTicket from '@/components/AirTicket'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'AirTicket',
+      component: AirTicket
     }
   ]
 })
